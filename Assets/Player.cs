a1d5c1c8cd98a86e71541c9408e4e16497dd88ca
unknown <molsem@920LAB03.cstcis.cti.depaul.edu>
2014-01-25 08:40:57 -0600

    First Push

---
+++ Assets/Player.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		int Mask; 	//1=
+					//2=
+					//3=
+					//4=
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
