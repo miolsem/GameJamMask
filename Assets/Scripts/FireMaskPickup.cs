56a052b490592db5958f1725e89eb0c7a0d8fde8
Mitch <miolsem@yahoo.com>
2014-01-26 04:29:45 -0600

    MASK OF TRUTH MOTHAFUCKASSSSSS

---
+++ Assets/Scripts/FireMaskPickup.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireMaskPickup : MonoBehaviour {
+	Player play;
+	// Use this for initialization
+	void Start () {
+		play = GameObject.Find ("First Person Controller").GetComponent<Player> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void OnMouseDown()
+	{
+		print ("firemask");
+		
+		play.fire = true;
+	}
+}
